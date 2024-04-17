8bc3a28ea0545720f0e3d023c2b14c52301505fb
Noman khan <Nomankkhan10047@gmail.com>
2024-04-17 17:55:03 +0500

    first commit

---
+++ assignment 4-5-6-7-8-9-10/main.js
@@ -0,0 +1,40 @@
+// assigment no 4
+var famousQoute = " A person who never made a mistake never tried anything new ";
+var author = "Albert Enstein";
+console.log("".concat(author, " once said, \"").concat(famousQoute, "\""));
+// assigment no 5
+var famousPerson = "Albert Enstein";
+var message = "".concat(author, " once said, \"").concat(famousQoute, "\"");
+console.log(message);
+// assigment no 6
+var nameWithWhiteSpace = "t\n\ Noman khan \t\n";
+console.log("name With White Space:", nameWithWhiteSpace);
+var strippedName = nameWithWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
+// assignment no 7
+var additionResul = 5 + 3;
+console.log("addition Resul:", additionResul);
+var subtractionResul = 10 - 2;
+console.log("subtraction Result:", subtractionResul);
+var multiplicationResult = 4 * 2;
+console.log("multiplication Resul:", multiplicationResult);
+var divitionResul = 16 / 2;
+console.log("divition Resullt:", divitionResul);
+// asssigment no 8
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+// assignment no 9
+var favoritedResul = 8;
+var messageResult = "My favorite number: ".concat(favoritedResul);
+console.log(messageResult);
+// Assignment no 10 
+// this program adds two number and print the result
+// define two numbers
+var number1 = 10;
+var number2 = 20;
+// add two numbers
+var result = number1 + number2;
+//print the resul
+console.log(result);
---
+++ assignment 4-5-6-7-8-9-10/main.ts
@@ -0,0 +1,58 @@
+// assigment no 4
+let famousQoute:string = " A person who never made a mistake never tried anything new ";
+let author:string = "Albert Enstein";
+
+console.log(`${author} once said, "${famousQoute}"`);
+
+// assigment no 5
+let famousPerson:string = "Albert Enstein";
+let message:string = `${author} once said, "${famousQoute}"`;
+
+console.log(message);
+
+// assigment no 6
+let nameWithWhiteSpace:string = "t\n\ Noman khan \t\n";
+console.log("name With White Space:",nameWithWhiteSpace);
+let strippedName:string = nameWithWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
+
+// assignment no 7
+let additionResul:number = 5+3;
+console.log("addition Resul:", additionResul);
+
+let subtractionResul:number = 10-2;
+console.log("subtraction Result:",subtractionResul);
+
+let multiplicationResult:number = 4*2;
+console.log("multiplication Resul:",multiplicationResult);
+
+let divitionResul:number = 16/2;
+console.log("divition Resullt:",divitionResul);
+
+// asssigment no 8
+console.log(5+3);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
+
+// assignment no 9
+let favoritedResul:number = 8;
+
+let messageResult:string = `My favorite number: ${favoritedResul}`;
+
+console.log(messageResult);
+
+
+// Assignment no 10 
+
+// this program adds two number and print the result
+
+// define two numbers
+let number1:number = 10;
+let number2:number = 20;
+// add two numbers
+let result:number = number1 + number2;
+//print the resul
+console.log(result);
+
+
